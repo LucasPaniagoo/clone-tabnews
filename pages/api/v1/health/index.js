d531d5a2338ae108d8162134284cf79c17320b4d
Lucas Paniago <lucaspaniagosantos@outlook.com>
2026-01-22 22:08:58 -0400

    finish `/api/v1/health` endpoint

--- pages/api/v1/health/index.js
+++ pages/api/v1/health/index.js
@@ -1,7 +1,27 @@
 import database from 'infra/database.js';
 
 export default async function health(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;")
-  console.log(result);
-  response.status(200).json({ chave: "valor"});
+  const updatedAt = new Date().toISOString();
+  const queryVersion = await database.query('SHOW server_version;');
+  const queryConnection = await database.query('SHOW max_connections;');
+  const databaseName = process.env.POSTGRES_DB;
+  const queryActiveConnection = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+
+  const dbVersion = queryVersion.rows[0].server_version;
+  const dbMaxConnections = queryConnection.rows[0].max_connections;
+  const dbOpenedConnections  = queryActiveConnection.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version:  dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: parseInt(dbOpenedConnections),
+      }
+    }
+  });
 }
